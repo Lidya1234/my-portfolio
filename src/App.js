8c5599d2dec8154641a7e657f364210addcfdbaf
Lidya1234 <lidya94gher@gmail.com>
2021-08-30 19:45:31 +0300

    Add menu and navbar components with their props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Navbar from "./components/Navbar/Navbar";
 import Intro from './components/Intro/Intro';
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from './components/Contact/Contact'
+import Menu from './components/Menu/Menu'
 import './app.scss';
-
+import {useState} from 'react'
 function App() {
+  const[menuOpen,setMenuOpen] =useState(false);
   return (
     <div className="app">
-     <Navbar />
+     <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className='sections'>
      <Intro />
      <Portfolio />
