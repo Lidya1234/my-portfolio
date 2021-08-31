f97ccfdaf2e26fa2d2f74d0d3db4c68ccdd1d83d
Lidya1234 <lidya94gher@gmail.com>
2021-08-31 19:40:31 +0300

    Add elements to portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,65 @@ import './Portfolio.scss';
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
+            <h1>Portfolio</h1>
+            <div className="container">
+                <div className='wrapper'>
+                <div className="item">
+                <a href="https://lidya1234.github.io/weather_app" target="_blank" rel='noreferrer'><img src="https://res.cloudinary.com/lidu4394/image/upload/v1616172130/zh4oqmk0aqbkrsoxixhk.png" alt="" /></a>
+                <h3>Weather App</h3>
+                </div>
+										<header>
+											
+											<a href="https://github.com/Lidya1234/weather_app" target="_blank" rel='noreferrer'>Code</a>
+											<h6>HTML|CSS|BOOTSTRAP|Javascript</h6>
+										</header>
+                </div>
+                <div className='wrapper'>
+                <div className="item">
+                <a href="https://jovial-agnesi-9bf50a.netlify.app/" target="_blank" rel='noreferrer'><img src="https://res.cloudinary.com/lidu4394/image/upload/v1621517710/vu-anh-TiVPTYCG_3E-unsplash_eivp1p.jpg" alt="images" /></a>
+                <h3>Find-your-house</h3>
+                </div>
+										<header>
+											
+											<a href="https://github.com/Lidya1234/find_my_house" target="_blank" rel='noreferrer'>Code</a>
+											<h6>React|ROR|CSS|BOOTSTRAP</h6>
+										</header>
+                </div>
+                <div className='wrapper'>
+                <div className="item">
+                <a href="https://my-tweeter-redesign.herokuapp.com/" target="_blank" rel='noreferrer'><img src="https://res.cloudinary.com/lidu4394/image/upload/v1616167294/te5vmhwba5lkf4ficeke.png" alt="twitter"  /></a>
+                <h3>Twitter redesign project</h3>
+                </div>
+										<header>
+											
+                                           <a href="https://github.com/Lidya1234/tweeter_redesign" target="_blank" rel='noreferrer'>Code</a>
+										   <h6>HTML|CSS|RUBY ON RAILS</h6>
+										</header>
+                </div>
+                <div className='wrapper'>
+                <div className="item">
+                <a href="https://romantic-albattani-29c047.netlify.app/" target="_blank" rel='noreferrer'><img src="https://res.cloudinary.com/lidu4394/image/upload/v1616172131/ggqbr03ot4v4rlrxb5kr.png"  alt="game" /></a>
+                <h3>Turn based RPG game</h3>
+                </div>
+										<header>
+								
+											<a href="https://github.com/Lidya1234/RPG-game" target="_blank" rel='noreferrer'>Code</a>
+											<h6>HTML|CSS|javascript|phaser</h6>
+										</header>
+                </div>
+                <div className='wrapper'>
+                <div className="item">
+                <a href="https://stayintouch1.herokuapp.com" class="image fit" target="_blank" rel='noreferrer'><img src="https://res.cloudinary.com/lidu4394/image/upload/v1616167107/zhklfoaz9dzpp0qcu57g.png" alt="" /></a>
+                <h3>Social Media</h3>
+                </div>
+										<header>
+										
+											<a href="https://github.com/Lidya1234/ror-social-scaffold" target="_blank" rel='noreferrer'>Code</a>
+											<h6>HTML|CSS|BOOTSTRAP|RUBY ON RAILS</h6>
+										</header>
+                </div>
+             
+            </div>
             
         </div>
     )
