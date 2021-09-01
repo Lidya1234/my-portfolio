61907ac63d7cf584f4a196a7a99cd769a8b34198
Lidya1234 <lidya94gher@gmail.com>
2021-09-01 19:34:53 +0300

    Add icons to navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Navbar.scss';
-import { Person, Mail } from '@material-ui/icons';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { Mail } from '@material-ui/icons';
 export default function Navbar({menuOpen, setMenuOpen}) {
     return (
         <div className={"navbar " + (menuOpen && "active")}>
@@ -8,8 +11,14 @@ export default function Navbar({menuOpen, setMenuOpen}) {
                 <div className='left'>
                  <a href='#intro' className='logo'>Lidu</a>
                  <div className='itemContainer'>
-                     <Person className='icon'/>
-                     <span>+256 759149629</span>
+                 <a href="https://github.com/Lidya1234"  target="_blank" rel='noreferrer'><GitHubIcon className='icon'/> </a>  
+                 </div>
+              
+                 <div className='itemContainer'>
+                 <a href="https://www.linkedin.com/in/lidya-ghebreigziabher/" target="_blank" rel='noreferrer'>< LinkedInIcon className='icon'/> </a>
+                 </div>
+                 <div className='itemContainer'>
+                 <a href="https://twitter.com/Lidya42676629" target="_blank" rel='noreferrer'><TwitterIcon className='icon'/></a>
                  </div>
                  <div className='itemContainer'>
                      <Mail className='icon'/>
