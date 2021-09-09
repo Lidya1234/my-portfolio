2eb198323ebeddbc64e84129c7d8eb9265992edf
Lidya1234 <lidya94gher@gmail.com>
2021-09-09 11:23:23 +0300

    Make the send button of the form fuctional,it can send messages

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 import './Contact.scss';
 
 export default function Contact() {
     const handleSubmit = (e) =>{
         e.preventDefault();
+        emailjs.sendForm('service_7l9sxh3','template_re9xp1m', e.target, 'user_CTKYvDQP1udBmu85MlM1T').then(res=>console.log(res)).catch(err=>console.log(err));
     }
     return (
         <div className='contact' id='contact'>
@@ -12,10 +14,10 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact</h2>
-    <form onClick={handleSubmit}>
-        <input type='text' placeholder='Email'/>
-        <textarea placeholder='Message'></textarea>
-        <button type='submit'>Send</button>
+    <form onSubmit={handleSubmit} >
+        <input type='text' placeholder='Email' name='email'/>
+        <textarea placeholder='Message' name='message'></textarea>
+        <input type='submit' value='Send' className='button'></input>
     </form>
             </div>
             
